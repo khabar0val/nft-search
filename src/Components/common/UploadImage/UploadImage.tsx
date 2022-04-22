ab04fa51d5126fc10c8cbe5fe2ce82bcbb324191
semgoSE <semgoSE@gmail.com>
2022-04-22 22:21:31 +0300

    style for upload imge

--- src/Components/common/UploadImage/UploadImage.tsx
+++ src/Components/common/UploadImage/UploadImage.tsx
@@ -11,10 +11,17 @@ interface FileImage {
 export const UploadImage = () => {
     const [file, setFile] = useState<FileImage | null>(null);
 
-    const onDrop = useCallback((acceptedFiles: any) => {
-        console.log(acceptedFiles);
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        if(acceptedFiles.length) {
+            const acceptedFile = acceptedFiles[0];
+            const image = URL.createObjectURL(acceptedFile);
+            setFile({
+                image,
+                file: acceptedFile
+            })
+        }
     }, [])
-    const { getInputProps, getRootProps } = useDropzone({ onDrop });
+    const { getInputProps, getRootProps, isDragActive } = useDropzone({ onDrop });
 
 
     return (
@@ -27,7 +34,7 @@ export const UploadImage = () => {
                         alt='Upload'
                         src={icon} 
                     />
-                    <p className='text'>Drag 'n' drop some files here, or click to select files</p>
+                    <p className='text'>{isDragActive ? "Drop here" : "Drag 'n' drop some files here, or click to select files"}</p>
                 </div> :
                 <div className='preview'>
                     <img src={file.image} alt='preview' />
