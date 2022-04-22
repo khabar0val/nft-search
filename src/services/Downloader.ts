bd9c99c21b793e90363ae276c4785b8f760710b3
Pavel068 <Pavel068>
2022-04-22 20:34:01 +0300

    Filter for image extensions

--- src/services/Downloader.ts
+++ src/services/Downloader.ts
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import axios from 'axios'
+import {Rarible} from "./Rarible";
 
 /**
  *
@@ -15,6 +16,10 @@ const download = async (url, name) => {
             responseType: 'stream',
         })
         const extension = data.headers['content-type'].split('/')[1]
+        if (['jpeg', 'jpg', 'png', 'gif', 'webp'].indexOf(extension) === -1) {
+            return null
+        }
+
         const filename = `${name}.${extension}`
         await data.data.pipe(
             fs.createWriteStream(path.resolve('files', filename))
