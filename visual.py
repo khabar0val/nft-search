b3030bad7a3c217d496fa5807bd5790d342cfdc5
Dmitriy <po4ta4dima@yandex.ru>
2022-04-28 11:10:02 +0300

    add visual.py

--- visual.py
+++ visual.py
@@ -2,5 +2,6 @@
 import cv2
 
 img_rgb = cv2.imread('img/COL.jpeg')
+resized = cv2.resize(img_rgb, (28, 28), interpolation = cv2.INTER_AREA)
 print(str(img_rgb))
 # template = cv2.imread('data/1.jpg')
